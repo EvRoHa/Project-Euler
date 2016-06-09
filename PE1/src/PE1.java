48a2c08994b58701473f0e173888e6a0a23b2cc8
Evan Hartman <Evan@Home>
2016-06-09 10:21:17 -0500

    Initial solution to PE1

---
+++ PE1/src/PE1.java
@@ -0,0 +1,30 @@
+/**
+ * The problem is to find the sum of all multiples of 3 or 5 less than 1000. We
+ * accomplish this by first adding all multiples of 3 and then adding all
+ * multiples of 5 that are not ALSO multiples of 3.
+ * 
+ * @author Evan Hartman
+ */
+
+public class PE1 {
+	/**
+	 * @param [String[]
+	 *            args] [Catches any initial arguments and ignores them.]
+	 * @return [Returns the sum of the all integer multiples of either 3 or 5
+	 *         that are less than 1000.]
+	 */
+	public static int main(String[] args) {
+		int sum = 0;
+		for (int i = 3; i < 999; i += 3) {
+			sum += i;
+		}
+		for (int i = 5; i < 1000; i += 5) {
+			if (i % 3 != 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		return sum;
+	}
+
+}
